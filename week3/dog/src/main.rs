1b18a9485f6ae536eaea2682b6f23497359727a3
Ryan Riley <rriley.cs@gmail.com>
2025-09-08 16:16:02 +0300

    Tweaking this demo to work more universally

--- week3/dog/src/main.rs
+++ week3/dog/src/main.rs
@@ -1,19 +1,25 @@
 struct Dog {
     name: String,
-    age: i32
+    age: i32,
 }
 
+// This is bad, because it takes ownership of the dog
 fn print_dog(dog: Dog) {
     println!("{} is {} years old", dog.name, dog.age);
 }
 
 fn better_print_dog(dog: Dog) -> Dog {
     println!("{} is {} years old", dog.name, dog.age);
-    return dog;
+    dog
 }
 
+// This borrows the dog struct
 fn best_print_dog(dog: &Dog) {
     println!("{} is {} years old", dog.name, dog.age);
+}
+
+fn age_dog(dog: &mut Dog) {
+    dog.age += 1;
 }
 
 fn main() {
@@ -21,24 +27,43 @@ fn main() {
         name: String::from("Fido"),
         age: 5,
     };
+    println!("{} is {} years old", my_dog.name, my_dog.age);
+    my_dog.age = 10;
+    println!("{} is {} years old", my_dog.name, my_dog.age);
 
-    // let other_dog = my_dog;
-    // println!("{} is {} years old", other_dog.name, other_dog.age);
-    // println!("{} is {} years old", my_dog.name, my_dog.age);
+    /*
+    // This doesn't work, because the assignment moves the ownership
+    let same_dog = my_dog;
+    println!("{} is {} years old", same_dog.name, same_dog.age);
+    println!("{} is {} years old", my_dog.name, my_dog.age);
+    */
 
-    // print_dog(my_dog);
-    // print_dog(my_dog);
+    /*
+    // This doesn't work, because the function call moves the ownership
+    print_dog(my_dog);
+    my_dog.age = 15;
+    */
+    
+    my_dog = better_print_dog(my_dog);
+    my_dog.age = 11;
+    my_dog = better_print_dog(my_dog);
 
-    // my_dog = better_print_dog(my_dog);
-    // my_dog = better_print_dog(my_dog);
+    best_print_dog(&my_dog);
+    my_dog.age = 12;
+    best_print_dog(&my_dog);
+
+    age_dog(&mut my_dog);
+    best_print_dog(&my_dog);
 
     let ptr1 = &my_dog;
     let ptr2 = &my_dog;
+
     best_print_dog(ptr1);
     best_print_dog(ptr2);
 
-    my_dog.age = 50;
+    /*
+    // This rule: Only one mutable borrow at a time, will drive you crazy
+    age_dog(&mut my_dog);
     best_print_dog(ptr1);
-    best_print_dog(ptr2);
-
+    */
 }
